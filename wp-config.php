4a42f6e05e59b32ff4f01064c88ffb12f0f6cacb
Tetaaard <74049094+Tetaaard@users.noreply.github.com>
2021-06-29 17:38:37 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,13 +22,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'wordpress_nishikigoi' );
+define( 'DB_NAME', 'votre_nom_de_bdd' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', 'votre_utilisateur_de_bdd' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'phpMyAdmi' );
+define( 'DB_PASSWORD', 'votre_mdp_de_bdd' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'localhost' );
@@ -53,14 +53,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '1-%_1tmiw b7iS]dK4^dC95rto+ G[B1wUw+0C3v^4h;,u;%,R/l_Ul!6)0dDkX!');
-define('SECURE_AUTH_KEY',  'M`>_+j@V9ijCmLADxp15C@tgMb}x7X!ojFRtI`zj&=kEUvktqF<v+r]9o01m$^6P');
-define('LOGGED_IN_KEY',    '(A({xQ$UrD8SvS53djWUu7O&J_2+-r]Vae?.+hSiTU-UIx7Tqx.#T5-)_GCrD;F~');
-define('NONCE_KEY',        '?)GL0BbJX1hrV%auPq:X,SPt+T;LU?8e(?<tg<769a}q[%,CwO=Q-rJa(07o[NR4');
-define('AUTH_SALT',        'E Js5m8BX=J?!j|-}.q]CPZp:HsK?swUP^7LZG~fQU$dgZuvkf3dq@iLf%Y<yRb^');
-define('SECURE_AUTH_SALT', 'qfmnpMhJZQ?shhx;-[jXO:f<wT-s/tia7-[(/Upu4r0KSphN6FHI=FW?g5LbFZU1');
-define('LOGGED_IN_SALT',   ']<ERc&Q=c (^=8)uilU)rj0Y5F$w^Y+[$~yvesDX.Zvx3wWbGXS(F%eQReZET:wI');
-define('NONCE_SALT',       '!db~%0z+c:mB^P$h>xo+el]!j+m}3H[E&qQdVJk`}i;A,XKJ$c@U&-DA*1}N4:Y/');
+define( 'AUTH_KEY',         'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_KEY',  'mettez une phrase unique ici' );
+define( 'LOGGED_IN_KEY',    'mettez une phrase unique ici' );
+define( 'NONCE_KEY',        'mettez une phrase unique ici' );
+define( 'AUTH_SALT',        'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_SALT', 'mettez une phrase unique ici' );
+define( 'LOGGED_IN_SALT',   'mettez une phrase unique ici' );
+define( 'NONCE_SALT',       'mettez une phrase unique ici' );
 /**#@-*/
 
 /**
@@ -96,5 +96,6 @@ if ( ! defined( 'ABSPATH' ) )
 
 /** Réglage des variables de WordPress et de ses fichiers inclus. */
 require_once( ABSPATH . 'wp-settings.php' );
+
 
 define('FS_METHOD', 'direct');
